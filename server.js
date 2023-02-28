57115411fff27f2a650063c0d4118225f50a53c4
indlukula <84289216+indlukula@users.noreply.github.com>
2023-02-28 06:45:28 +0200

    rename swaggerPU

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-.use(cors())
+  .use(cors())
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');

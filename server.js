220df82014fb0084c542622523c6d9b778d7d6c8
indlukula <84289216+indlukula@users.noreply.github.com>
2023-02-28 09:03:45 +0200

    updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
-const mongodb = require('./config/db.config');
 const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
   .use(cors())
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(express.json())
   .use(bodyParser.json())
+  .use(express.urlencoded({ extended: true }))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -20,14 +26,14 @@ app
   })
   .use('/', require('./routes'));
 
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-
-  } else {
-    app.listen(port);
-    console.log(`DB Connected and server running on ${port}.`);
-
-  }
-});
+  mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
   
+    } else {
+      app.listen(port);
+      console.log(`DB Connected and server running on ${port}.`);
+  
+    }
+  });
+    
